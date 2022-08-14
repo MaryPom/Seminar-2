c3fdcdb29fbda0e54aea99d49a859740733af066
Mariia <mashulkaegor@mail.ru>
2022-08-14 17:24:04 +0400

    add Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 5. Найти кубы чисел от 1 до N
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+while(count <= N)
+{
+    Console.WriteLine(count * count * count);
+    count++;
+}
