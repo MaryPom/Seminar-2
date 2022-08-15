c1accfe324d64a250b927899f5f4b3a5dbd94a6d
Mariia <mashulkaegor@mail.ru>
2022-08-15 14:25:31 +0400

    add Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program {
+ public static int Main() {
+  Console.WriteLine("Введите число : ");
+  int n =int.Parse(Console.ReadLine());
+  int i = 0;
+  while(n > 0) 
+  {
+   i++;
+   n/=10;
+  }
+  Console.WriteLine("Количество цифр введенного числа : {0}", i);
+  Console.ReadKey();
+  return 0;
+ }
+}
