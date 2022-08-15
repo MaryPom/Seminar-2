75dd20b600826a20c004e825d139a0b9e2b1239b
Mariia <mashulkaegor@mail.ru>
2022-08-15 19:42:36 +0400

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10. Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= N; i += 2)
+{
+    Console.WriteLine(i * i * i);
+}
