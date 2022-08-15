836959262b2cb7117d928d5ddb01abd87164c805
Mariia <mashulkaegor@mail.ru>
2022-08-15 19:49:53 +0400

    add Task 7/1

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,15 +1,11 @@
-﻿class Program {
- public static int Main() {
-  Console.WriteLine("Введите число : ");
-  int n =int.Parse(Console.ReadLine());
-  int i = 0;
-  while(n > 0) 
-  {
-   i++;
-   n/=10;
-  }
-  Console.WriteLine("Количество цифр введенного числа : {0}", i);
-  Console.ReadKey();
-  return 0;
- }
+﻿// Задача 7. Определить количество цифр в числе
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int result = 0;
+
+while(n >= 1)
+{
+    n = n / 10;
+    result ++;
 }
+Console.WriteLine(result);
