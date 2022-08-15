e0ad57c5b4dba0055ef9db145c30c5e2d6773147
Mariia <mashulkaegor@mail.ru>
2022-08-15 15:55:29 +0400

    add Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 9. Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int x = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    x = x * i;
+}
+Console.WriteLine(x);
